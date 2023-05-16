0233dc31235341f2cadc410357e4cbb07524f619
Hasib Hossain <boraborhasib@gmail.com>
2023-05-16 16:18:02 +0600

    User navbar added

---
+++ Client/src/component/user/Unav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/logo/logo.png';
+// U navbar mining user navbar
+const Unav = () => {
+    return (
+        <div className='py-6 flex justify-center items-center '>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Unav;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './component/home/Home'
+import Login from './component/user/Login'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {path:'/',element:<Home />},
     ]
   },
+  {
+    path: '/login',
+    element: <Login />,
+  }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
